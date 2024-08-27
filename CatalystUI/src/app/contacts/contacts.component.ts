d487c38062128e46a852acb5212f86ef8de6c341
jmarkmcdow <jmarkmcdow@hotmail.com>
2024-08-27 09:16:13 -0700

    Think I am finally ready to start next angular module

--- CatalystUI/src/app/contacts/contacts.component.ts
+++ CatalystUI/src/app/contacts/contacts.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { IContact } from './contact.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'cnw-contacts',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
 export class ContactsComponent {
+  contacts: IContact[];
+  filter: string='';
 
+  constructor(){
+    this.contacts = [
+      {
+        id: 1,
+        firstname: 'Tina',
+        lastname: 'Turner',
+        comments: 'Legend',
+        age: 70,
+        address: '334 Main St. Philadelphia, PA 88888',
+        occupation: 'Vocalist',
+        business: 'Music'
+      },
+      {
+        id: 77,
+        firstname: 'James',
+        lastname: 'Mason',
+        comments: 'Legend',
+        age: 90,
+        address: '333 Angel Ave, Paradise, MO',
+        occupation: 'Actor',
+        business: 'Fox Studios',
+      }
+    ]
+    this.contacts.forEach(e => {
+      e.name = `${e.firstname} ${e.lastname}`
+    });
+  }
+  getImageUrl (contact: IContact){
+    return '/assets/images/${contact.firstname + contact.lastname}'
+  }
+
+  getFilteredContacts(){
+    return this.filter === '' 
+    ? this.contacts 
+    : this.contacts.filter((c) => c.lastname === this.filter);
+  }
 }
---
+++ CatalystUI/src/app/site-header/site-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cnw-site-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './site-header.component.html',
+  styleUrls: ['./site-header.component.css']
+})
+export class SiteHeaderComponent {
+  constructor() {}
+}
